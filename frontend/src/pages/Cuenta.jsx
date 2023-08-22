bbc2e88b8bb784bef44fcc7748c9508816c27bbe
Lucas Brest <lucasezequiel237@gmail.com>
2023-08-22 17:28:31 -0300

    Add CuentaPage, falta reenderizar info y llamados al backend

--- frontend/src/pages/Cuenta.jsx
+++ frontend/src/pages/Cuenta.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useAuth } from './../contexts/auth'
 
 const Cuenta = () => {
+  const { user } = useAuth()
+
+  useEffect(() => {
+    console.log(user);
+  }, [])
+
+  if(!user) return <span>Error</span>
+
   return (
     <div>Cuenta</div>
   )
